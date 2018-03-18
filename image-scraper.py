3a0f3b768febfe1be9f176808ddf5b552cc1da8c
kkas <kentakikui@gmail.com>
2018-03-18 20:21:36 +0900

    Add returning multiple image urls

--- image-scraper.py
+++ image-scraper.py
@@ -1,4 +1,6 @@
-import re, requests, json
+import re
+import requests
+import json
 import sys
 
 from bs4 import BeautifulSoup
@@ -37,19 +39,23 @@ def instagram():
     # Retrieve the contents by making GET request to the URL.
     content = do_get(target_url)
 
-    # Parse the content and retrieve the expected url for the picture.
-    picture_url = retrieve_picture_url(target_url, content)
+    # Parse the content and extract the json part.
+    json_obj = extract_json_part(content)
+
+    # Retrieve the image resources.
+    image_resources = extract_image_resources(json_obj)
+
+    print("image_resources(before): {}".format(image_resources), file=sys.stderr)
 
     # Modify the url to avoid Instagram's "URL signature expired" problem.
     # See the following post for more details.
     # https://stackoverflow.com/questions/47668014/instagram-feed-api-media-url-shows-url-signature-expired
-    fixed_picture_url = make_permanent_url(picture_url)
+    image_resources = make_permanent_urls(image_resources)
 
-    print("original_url: {}".format(picture_url), file=sys.stderr)
-    print("url: {}".format(fixed_picture_url), file=sys.stderr)
+    print("image_resources(after): {}".format(image_resources), file=sys.stderr)
 
-    return jsonify(original_url=picture_url,
-                   url=fixed_picture_url,
+    return jsonify(original_url=target_url,
+                   image_resources=image_resources,
                    status='SUCCESS')
 
 
@@ -58,35 +64,36 @@ def do_get(url):
     return requests.get(url).text
 
 
+def make_permanent_urls(image_resources):
+    for resource in image_resources:
+        resource['src'] = make_permanent_url(resource['src'])
+    return image_resources
+
+
 def make_permanent_url(url):
     """Modify the original url of a picture on Instagram to avoid the 'URL signature expired' problem. """
     return re.sub(r'/vp.*\/.{32}\/.{8}\/', '/', url)
 
-def retrieve_picture_url(url, content):
-    soup = BeautifulSoup(content, "html.parser")
 
-    # The Json part is embedded inside of the <script> tag.
-    # The script declares a variable, '_sharedData' and sets the json data for it.
-    script_text = soup.find('script', type='text/javascript', string=re.compile('_sharedData')).string
-
-    json_obj = extract_json(script_text)
-    picture_url = json_obj['entry_data']['PostPage'][0]['graphql']['shortcode_media']['display_url']
-
-    return picture_url
+def extract_image_resources(json_obj):
+    return json_obj['entry_data']['PostPage'][0]['graphql']['shortcode_media']['display_resources']
 
 
 def is_instagram_url(url):
     return re.match(INSTAGRAM_URL_PREFIX, url)
 
 
-def extract_json(script_text):
-    json_text = extract_json_part(script_text)
+def extract_json_part(content):
+    soup = BeautifulSoup(content, "html.parser")
+
+    # The Json part is embedded inside of the <script> tag.
+    # The script declares a variable, '_sharedData' and sets the json data for it.
+    script_text = soup.find('script', type='text/javascript', string=re.compile('_sharedData')).string
+
+    # Remove any unnecessary parts and extract only the json part.
+    json_text = re.sub(r'(?:window._sharedData = |;$)', "", script_text)
+
     return json.loads(json_text)
-
-
-def extract_json_part(script_text):
-    """Remove any unnecessary parts and extract only the json part."""
-    return re.sub(r'(?:window._sharedData = |;$)', "", script_text)
 
 
 if __name__ == '__main__':
